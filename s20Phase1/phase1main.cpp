59e995a5bfe8ea0b4b734e1f199bb3710602db75
cciwuaba <cciwuaba@crimson.ua.edu>
2020-02-23 21:09:20 -0600

    new tester

--- s20Phase1/phase1main.cpp
+++ s20Phase1/phase1main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 #include "CDA.cpp"
+
+void test1(ostream &fp);
+void test2(ostream &fp);
+void test3(ostream &fp);
+void test4(ostream &fp);
+void test5(ostream &fp);
+void test6(ostream &fp);
 
 void foo(CDA<int> x) {
 	for (int i=0; i<x.Length()/2; i++){
@@ -12,64 +21,172 @@ void foo(CDA<int> x) {
 	// x => "5 6 7 8 9 1 1 2 3 4 10" SetOrdered => -1	
 }
 
-int main(){
-	CDA<float> C(10);
-	for (int i=0; i< C.Length();i++) C[i] = i;
-	for (int i=0; i< C.Length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "0 1 2 3 4 5 6 7 8 9"
+int printWrapper = 0;
+
+int main(int argc, char **argv){
+	int testToRun = atoi(argv[1]);
+	switch (testToRun){
+		case 1:
+			test1(cout);
+			break;
+		case 2:
+			test2(cout);
+			break;
+		case 3:
+			test3(cout);
+			break;
+		case 4:
+			test4(cout);
+			break;
+		case 5:
+			test5(cout);
+			break;
+		case 6:
+			test6(cout);
+			break;
+	}
+}
+
+void test1(ostream &fp){
+	if(printWrapper) fp << "Running test 1" << endl;
+	//fp.open("test1.out");
+	CDA<int> C(25);
+	for (int i=0; i< C.Length();i++) C[i] = i*10;
+	for (int i=0; i< C.Length();i++) fp << C[i] << " "; fp << endl;
 	C.DelFront();
-	for (int i=0; i< C.Length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "1 2 3 4 5 6 7 8 9"
+	C.DelFront();
+	for (int i=0; i< C.Length();i++) fp << C[i] << " ";  fp << endl;
 	C.DelEnd();
-	for (int i=0; i< C.Length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "1 2 3 4 5 6 7 8"
-	C.AddEnd(100.0);
-	for (int i=0; i< C.Length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "1 2 3 4 5 6 7 8 100"
-	C.AddEnd(101.0);
-	for (int i=0; i< C.Length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "1 2 3 4 5 6 7 8 100 101"
+	C.DelEnd();
+	for (int i=0; i< C.Length();i++) fp << C[i] << " ";  fp << endl;
+	C.AddEnd(1000);
+	C.AddEnd(1002);
+	for (int i=0; i< C.Length();i++) fp << C[i] << " ";  fp << endl;
+	C.AddFront(1001);
+	C.AddEnd(1003);
+	for (int i=0; i< C.Length();i++) fp << C[i] << " ";  fp << endl;
 	C.DelEnd(); C.DelEnd();
-	C.AddFront(-100.0); C.AddFront(-200.0);
-	for (int i=0; i< C.Length();i++) cout << C[i] << " ";  cout << endl;
-	// C => "-200 -100 1 2 3 4 5 6 7 8"
+	C.AddFront(-101); C.AddFront(-201);
+	for (int i=0; i< C.Length();i++) fp << C[i] << " ";  fp << endl;
+	fp << "Capacity is " << C.Capacity() << endl;
 
 	CDA<int> A;
-	for(int i=0; i<10;i++) A.AddEnd(i);
-	cout << "SetOrdered is " << A.SetOrdered() << endl;
-	// A => "0 1 2 3 4 5 6 7 8 9" SetOrdered => 1
-	cout << "Select is " << A.Select(3) << endl;
-	// A => "0 1 2 3 4 5 6 7 8 9" Select => 2
-	cout << "Search is " << A.Search(5) << endl;
-	// A => "0 1 2 3 4 5 6 7 8 9" Search => 5
-	A.AddFront(10); 
-	// A => "10 0 1 2 3 4 5 6 7 8 9"
-	cout << "SetOrdered is " << A.SetOrdered() << endl;
-	// A => "10 0 1 2 3 4 5 6 7 8 9" SetOrdered => -1
-	cout << "Select is " << A.Select(3) << endl;
-	// A => "10 0 1 2 3 4 5 6 7 8 9" Select => 2
-	cout << "Search is " << A.Search(5) << endl;
-	for (int i=0; i< A.Length();i++) cout << A[i] << " ";  cout << endl;
-	// A => "10 0 1 2 3 4 5 6 7 8 9" Search => 5
+	int temp = 0;
+	for(int i=0; i<17;i++){
+		A.AddEnd(i*10);
+		if(A.Capacity() != temp){
+			temp = A.Capacity();
+			fp << "Capacity is " << temp << " ";
+		}
+	}
+    fp << endl;
+	fp << "Capacity is " << A.Capacity() << endl;
+	fp << "Select is " << A.Select(5) << endl;
+	fp << "Select is " << A.Search(150) << endl;
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	A.AddFront(11); 
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Select is " << A.Select(5) << endl;
+	fp << "Select is " << A.Search(150) << endl;
 	A.InsertionSort();
-	// A => "0 1 2 3 4 5 6 7 8 9 10"
-	cout << "SetOrdered is " << A.SetOrdered() << endl;
-	// A => "0 1 2 3 4 5 6 7 8 9 10" SetOrdered => 1
-	A.AddEnd(-1);
-	// A => "0 1 2 3 4 5 6 7 8 9 10 -1"
-	cout << "Ordered is " << A.Ordered() << endl;
-	// A => "0 1 2 3 4 5 6 7 8 9 10 -1" Ordered => False
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	A.AddEnd(-10);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
 	A.QuickSort();
-	// A => "-1 0 1 2 3 4 5 6 7 8 9 10"
-	cout << "Ordered is " << A.Ordered() << endl;
-	// A => "-1 0 1 2 3 4 5 6 7 8 9 10" Ordered => True
-	A.DelFront(); A.DelFront(); A.AddEnd(1);
-	// A => "1 2 3 4 5 6 7 8 9 10 1"
-	A.CountingSort(10);
-	// A => "1 1 2 3 4 5 6 7 8 9 10"
-	for (int i=0; i< A.Length();i++) cout << A[i] << " ";  cout << endl;
-
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	A.DelFront(); A.DelFront(); A.AddEnd(52);
+	for (int i=0; i< A.Length();i++) fp << A[i] << " ";  fp << endl;
+	A.CountingSort(200);
+	for (int i=0; i< A.Length();i++) fp << A[i] << " ";  fp << endl;
+	CDA<int> B;
+	B = A;
+	A[0] = -1001;
+	A[1] = -1003;
+	fp << B[0] << endl;
 	foo(A);
-	cout << "Ordered is " << A.Ordered() << endl;
-	// A => "1 1 2 3 4 5 6 7 8 9 10" Ordered => True
+	for (int i=0; i< B.Length();i++) fp << B[i] << " ";  fp << endl;
+	CDA<long long int> testGeneric1(10);
+	CDA<wchar_t> testGeneric2(10);
+	CDA<long double> testGeneric3(10);
+	fp << "Passed Generics Test" << endl;
+	//fp.close();
+	if(printWrapper)fp << "-----------------Finished Test----------------------" << endl;
 }
+
+void test2(ostream &fp){
+	if(printWrapper) fp << "Running test 2" << endl;
+	CDA<int> A;
+	for(int i = 0; i < 99000; i++) A.AddFront(i);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Select is " << A.Select(8000) << endl;
+	A.QuickSort();
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Search is " << A.Search(8000) << endl;
+	fp << "Select is " << A.Select(800) << endl;
+	if(printWrapper) fp << "-----------------Finished Test----------------------" << endl;
+}
+
+void test3(ostream &fp){
+	if(printWrapper) fp << "Running test 3" << endl;
+	CDA<int> A;
+	for(int i = 0; i < 10000; i++) A.AddFront(i);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Select is " << A.Select(8000) << endl;
+	A.InsertionSort();
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Search is " << A.Search(8000) << endl;
+	fp << "Select is " << A.Select(800) << endl;
+	if(printWrapper) fp << "-----------------Finished Test----------------------" << endl;
+}
+
+void test4(ostream &fp){
+	if(printWrapper) fp << "Running test 4" << endl;
+	CDA<int> A;
+	for(int i = 0; i < 10000; i++) A.AddFront(i);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Select is " << A.Select(8000) << endl;
+	A.CountingSort(10000);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Search is " << A.Search(8000) << endl;
+	fp << "Select is " << A.Select(800) << endl;
+	if(printWrapper) fp << "-----------------Finished Test----------------------" << endl;
+}
+
+
+void test5(ostream &fp){
+	if(printWrapper) fp << "Running test 5" << endl;
+	CDA<int> A;
+	for(int i = 0; i < 10000; i++) A.AddFront(i*2);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	for(int i = 1; i <= 15; i++)
+		fp << "Select is " << A.Select(1000+i*2) << " "; 
+	fp << endl;
+	A.CountingSort(20000);
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	for(int i = 1; i <= 15; i++){		
+		fp << "Select is " << A.Select(i*2) << " ";
+	} 
+	fp << endl;
+	for(int i = 1; i <= 15; i++){		
+		fp << "Search is " << A.Search(i*2) << " ";
+	} 
+	fp << endl;
+	if(printWrapper) fp << "-----------------Finished Test----------------------" << endl;
+}
+
+
+void test6(ostream &fp){
+	if(printWrapper) fp << "Running test 6" << endl;
+	CDA<int> A;
+	for(int i = 0; i < 100000; i++) A.AddFront(i*2);
+	A.QuickSort();
+	fp << "SetOrdered is " << A.SetOrdered() << endl;
+	fp << "Select is " << A.Select(8000) << endl;
+	fp << "Search is " << A.Search(80) << endl;
+	fp << "Search is " << A.Search(800) << endl;
+	fp << "Search is " << A.Search(8000) << endl;
+	fp << "Search is " << A.Search(80001) << endl;
+	if(printWrapper) fp << "-----------------Finished Test----------------------" << endl;
+}
+
+
